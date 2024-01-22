be4380dc93ac8529fded47e3645824a329688dd4
omegaeducacional <86612499+omegaeducacional@users.noreply.github.com>
2024-01-22 13:35:46 +0000

    adicionado botão de ir para o topo

--- custom.js
+++ custom.js
@@ -1,3 +1,51 @@
+function toTopButton() {
+    $("body").append(`
+        <a class="to-top hide" href="" id="js-top">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
+            <span class="screen-reader-text">Ir para o topo</span>
+        </a>
+    `);
+
+    // Set a variable for our button element.
+    const scrollToTopButton = document.getElementById('js-top');
+
+    // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+    const scrollFunc = () => {
+        // Get the current scroll value
+        let y = window.scrollY;
+
+        // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+        if (y > 0) {
+            scrollToTopButton.className = "top-link show";
+        } else {
+            scrollToTopButton.className = "top-link hide";
+        }
+    };
+
+    window.addEventListener("scroll", scrollFunc);
+
+    const scrollToTop = () => {
+        // Let's set a variable for the number of pixels we are from the top of the document.
+        const c = document.documentElement.scrollTop || document.body.scrollTop;
+
+        // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+        // We'll also animate that scroll with requestAnimationFrame:
+        // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+        if (c > 0) {
+            window.requestAnimationFrame(scrollToTop);
+            // ScrollTo takes an x and a y coordinate.
+            // Increase the '10' value to get a smoother/slower scroll!
+            window.scrollTo(0, c - c / 10);
+        }
+    };
+
+    // When the button is clicked, run our ScrolltoTop function above!
+    scrollToTopButton.onclick = function (e) {
+        e.preventDefault();
+        scrollToTop();
+    }
+}
+
 (function () {
     'use strict';
 
@@ -25,15 +73,16 @@
         });
     }, 150);
 
-    
+
     setTimeout(() => {
         $(".comment-warning-wrapper").before(`
             <div class="omg-issue-toolbar">
                 <label for="commands_omega">Templates</label>
                 <select id="commands_omega">
-                    <optgroup label="Comentário padrão">
-                    <option value='/apply_template retorno_dev2'>Retorno para qualidade(Dev desktop)</option>
+                    <option value=''>Selecione um template</option>
+                    <optgroup label="Retornos">
                     <option value='/apply_template retorno_dev'>Retorno para qualidade(Dev web)</option>
+                    <option value='/apply_template retorno_dev2'>Retorno para qualidade(Dev desktop)</option>
                     <optgroup label="Templates execução bot">
                     <option value='/create_branch feature'>Cria galho feature</option>
                     <option value='/create_branch hotfix'>Cria galho hotfix</option>
@@ -51,14 +100,8 @@
             } else {
                 note.val(this.value);
             }
-         });
+        });
 
-        $("body").after(`<link rel="stylesheet" href="https://omegaeducacional.github.io/scripts_custom/board-enhancement/board-enhancement.css"></link>
-         <script src="https://omegaeducacional.github.io/scripts_custom/board-enhancement/board-enhancement.js"></script>
-        `);
+        toTopButton();
     }, 150)
-    
-    
-    
-
 })();
