36fd88dd5648b28102030bd5ef7ce92f311732f7
omegaeducacional <86612499+omegaeducacional@users.noreply.github.com>
2024-02-08 21:59:09 +0000

    fix

--- custom.js
+++ custom.js
@@ -86,7 +86,7 @@ function toTopBottomButton() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
             <span class="screen-reader-text">Ir para baixo</span>
         </a>
-        <img src="https://www.movidesk.com/assets/pt-br/img/logo-movidesk-zenvia.svg" class="img-fluid" width="205" height="55" alt="Movidesk" class="openTicket">
+        <img src="https://www.movidesk.com/assets/pt-br/img/logo-movidesk-zenvia.svg" class="img-fluid openTicket" width="160" alt="Movidesk tickets">
     `);
 
     // Set a variable for our button element.
