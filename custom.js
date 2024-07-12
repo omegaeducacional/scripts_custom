ffaa11c56553bdf23cc00489b1fd0c99c07fa51f
omegaeducacional <86612499+omegaeducacional@users.noreply.github.com>
2024-07-12 09:55:21 -0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -77,6 +77,9 @@ function buttonCommentClick () {
 }
 
 function toTopBottomButton() {
+    if (window.$ === undefined) {
+        return;
+    }
     $("body").append(`
         <a class="to-top hide" href="">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
